7b85211f7ab210bd0b805e4519a1fc8fa43eb21a
LuisPati <126108089+LuisPati11@users.noreply.github.com>
2023-05-09 13:59:01 +0300

    Version 1 repositorio terminada

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-const mainCursor= document.querySelector('.main');
-
-mainCursor.addEventListener('mousemouve',() =>{
-    console.log("Moviendo")
-})
-
 var boton = document.getElementById("copiador");
 boton.addEventListener("click", copiarAlPortapapeles, false);
 function copiarAlPortapapeles() {
@@ -14,4 +8,16 @@ function copiarAlPortapapeles() {
   inputFalso.select();
   document.execCommand("copy");
   document.body.removeChild(inputFalso);
+}
+
+var check=document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+  let id=check.checked;
+  if(id==true){
+    location.href="en/index.html";
+  }else{
+    location.href="../index.html";
+  }
 }
